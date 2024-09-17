15e9a037ce0e1b914d1a3b14b0e0995da1de2fb1
felipe <neivajansen.felipe@gmail.com>
2024-09-17 02:11:49 -0300

    API teste técnico AON.
    Configurando Dockerfile API.

--- api-usuarios/config/database.js
+++ api-usuarios/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/usuarios', {
+        await mongoose.connect('mongodb://mongodb:27017/usuarios', {
 
             useNewUrlParser: true,
             useUnifiedTopology: true,
